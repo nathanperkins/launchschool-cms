f89da122a05401f4b2978f4d25d71bcf3df4b837
Nathan Perkins <nperkins487@gmail.com>
2016-03-23 04:49:05 +0000

    done

--- cms.rb
+++ cms.rb
@@ -6,12 +6,22 @@ if development?
   require 'sinatra/reloader'
 end
 
-root = File.expand_path('..', __FILE__)
+ROOT = File.expand_path('..', __FILE__)
 
 get '/' do
-  @files = Dir.entries(root + '/data')
+  @files = Dir.entries(ROOT + '/data')
   @files.select! { |file| !File.directory? file }
   @files.sort!
 
   erb :index
+end
+
+get '/:file_name' do
+  headers['Content-Type'] = 'text/plain'
+
+  File.read file_path(params[:file_name])
+end
+
+def file_path(file_name)
+  ROOT + '/data/' + params[:file_name]
 end
