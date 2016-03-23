aa6436a29383caa867fff76786db81eacb6f90da
Nathan Perkins <nperkins487@gmail.com>
2016-03-23 08:02:08 +0000

    slight refactoring

--- cms.rb
+++ cms.rb
@@ -7,9 +7,38 @@ if development?
   require 'sinatra/reloader'
 end
 
-set :sessions, true
+configure do
+  enable :sessions
+  set :session_secret, 'super secret'
+end
 
 ROOT = File.expand_path('..', __FILE__)
+
+def file_path(file_name)
+  ROOT + '/data/' + file_name
+end
+
+def load_file_content(path)
+  content = File.read(path)
+  case File.extname(path)
+  when '.md'
+    render_markdown content
+  when '.txt'
+    headers['Content-Type'] = 'text/plain'
+    content
+  end
+end
+
+def data_files
+  @files = Dir.entries(ROOT + '/data')
+  @files.select! { |file| !File.directory? file }
+  @files.sort!
+end
+
+def render_markdown(text)
+  markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML)
+  markdown.render(text)
+end
 
 get '/' do
   @files = data_files
@@ -21,30 +50,10 @@ get '/:file_name' do
   file_name = params[:file_name]
 
   if File.exist? file_path(file_name)
-    if file_name =~ /.md/
-      render_markdown file_name
-    else
-      headers['Content-Type'] = 'text/plain'
-      File.read(file_path(file_name))
-    end
+    load_file_content(file_path(file_name))
   else
     session[:message] = "#{file_name} does not exist."
 
     redirect '/'
   end
-end
-
-def file_path(file_name)
-  ROOT + '/data/' + file_name
-end
-
-def data_files
-  @files = Dir.entries(ROOT + '/data')
-  @files.select! { |file| !File.directory? file }
-  @files.sort!
-end
-
-def render_markdown(file_name)
-  markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML)
-  markdown.render File.read(file_path(file_name))
 end
